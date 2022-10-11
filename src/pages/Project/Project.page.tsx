ea251bc4f019bf90e38017a51669d23ab1025acf
Mateusz <nowe7246@gmail.com>
2022-10-11 18:36:18 +0200

    Created project page with first display project.

--- src/pages/Project/Project.page.tsx
+++ src/pages/Project/Project.page.tsx
@@ -1,13 +1,19 @@
+import {ProjectCard} from "../../components/Project/ProjectCard";
+import {ProjectData} from "../../components/Project/project.data";
 import {ProjectContainer} from "../../components/Project/Project.styles";
 
-import {ProjectCard} from "../../components/Project/ProjectCard";
 
-
-export const ProjectPage = () => {
-    return (
-        <ProjectContainer>
-            <h2>Dotychczasowe projekty</h2>
-            <ProjectCard/>
-        </ProjectContainer>
-    )
-}
+export const ProjectPage = () => (
+    <ProjectContainer>
+        <h2>Dotychczasowe projekty</h2>
+        {ProjectData.map(project => (
+            <ProjectCard
+                name={project.name}
+                image={project.image}
+                description={project.description}
+                links={project.links}
+                demo={project.demo}
+            />
+        ))}
+    </ProjectContainer>
+)
