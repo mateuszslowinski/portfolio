51e910145a38452988d9fa54d9c3e60ac0984c85
Mateusz <nowe7246@gmail.com>
2022-10-11 18:34:14 +0200

    Created project data file with info about single project.

---
+++ src/components/Project/project.data.ts
@@ -0,0 +1,21 @@
+export const ProjectData = [
+    {
+        name:'TeaShop',
+        image:'/images/projectImages/TeaShop.png',
+        description:'opis jakis tam',
+        demo:{
+            url:'https://tea-shop-front.herokuapp.com',
+        },
+        links:[
+            {
+                name:'frontend',
+                url:'https://github.com/mateuszslowinski/teaShopFront',
+            },
+            {
+                name:'backend',
+                url:'https://github.com/mateuszslowinski/teaShop-backend',
+            }
+        ],
+    },
+
+]
