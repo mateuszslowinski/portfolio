1280dfa4f8c2cc38a56c2ebfb4d8b4d4eef731cd
Mateusz <nowe7246@gmail.com>
2022-10-16 11:11:48 +0200

    Added flex wrap to page and layout containers and 100vw to width in page container.

--- src/components/Layout/Layout.styles.ts
+++ src/components/Layout/Layout.styles.ts
@@ -1,14 +1,15 @@
 import styled from "styled-components";
 
-
 export const LayoutContainer = styled.div`
   display: flex;
   flex-direction: column;
+  flex-wrap: wrap;
 `
-
 export const PageContainer = styled.div`
   display: flex;
   flex-direction: column;
+  flex-wrap: wrap;
+  width: 100vw;
   max-width: 1400px;
   margin: 10px auto;
 `
