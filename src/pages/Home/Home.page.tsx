e2952fe1daad5d964c08c1776e02e0be508bf6f7
Mateusz <nowe7246@gmail.com>
2022-10-30 09:24:25 +0100

    Added animation opening home page.

--- src/pages/Home/Home.page.tsx
+++ src/pages/Home/Home.page.tsx
@@ -1,7 +1,13 @@
+import {motion} from "framer-motion";
 import {Home} from "../../components/Home/Home";
 
 export const HomePage = () => (
-    <>
+    <motion.div
+        initial={{y: "100%"}}
+        animate={{y: "0%"}}
+        exit={{opacity: 1}}
+        transition={{duration: 0.4, ease: 'easeOut'}}
+    >
         <Home/>
-    </>
+    </motion.div>
 )
