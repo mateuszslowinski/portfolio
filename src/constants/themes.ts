76ccc6a2265ffff1caba62faff37f2b7fa370310
Mateusz <nowe7246@gmail.com>
2022-10-31 12:00:35 +0100

    Created themes file with of values of theme styles.

---
+++ src/constants/themes.ts
@@ -0,0 +1,15 @@
+import {ThemeProps} from "../types/layout.types";
+
+export const darkTheme:ThemeProps = {
+    backGroundColor: '#222224',
+    fontColor: 'whitesmoke',
+    hoverColors:'#222224',
+}
+
+export const lightTheme:ThemeProps = {
+    backGroundColor: 'whitesmoke',
+    fontColor: '#222224',
+    hoverColors:'#0B8BD4',
+
+}
+
