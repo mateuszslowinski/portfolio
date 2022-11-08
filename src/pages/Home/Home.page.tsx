45da1b98afc1e6adf2472824716f86fdbe5bb317
Mateusz <nowe7246@gmail.com>
2022-11-08 18:21:47 +0100

    Changed animation in home page.

--- src/pages/Home/Home.page.tsx
+++ src/pages/Home/Home.page.tsx
@@ -3,10 +3,10 @@ import {Home} from "../../components/Home/Home";
 
 export const HomePage = () => (
     <motion.div
-        initial={{y: "100%"}}
-        animate={{y: "0%"}}
-        exit={{opacity: 1}}
-        transition={{duration: 0.4, ease: 'easeOut'}}
+        initial={{opacity: 0, y: 200}}
+        animate={{opacity: 1, y: 0}}
+        exit={{opacity: 0, y: -200}}
+        transition={{ease: 'easeInOut', duration: 1}}
     >
         <Home/>
     </motion.div>
